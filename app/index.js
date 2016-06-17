66e7e061296f851789719f6b921ff23fba9943d6
John-David Dalton <john.david.dalton@gmail.com>
2016-06-17 16:57:09 -0700

    Cleanup `getShippingInfo`.

--- app/index.js
+++ app/index.js
@@ -19,13 +19,11 @@ new Vue({ // eslint-disable-line no-new
     response: {},
   },
   methods: {
-    getShippingInfo: function() {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', [BASE_API_URL, this.carrier, this.trackingNum].join('/'), true);
+    getShippingInfo: () => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', `${BASE_API_URL}/${this.carrier}/${this.trackingNum}`, true);
       xhr.withCredentials = false;
-      xhr.onload = () => {
-        this.response = JSON.parse(xhr.responseText);
-      }
+      xhr.onload = () => this.response = JSON.parse(xhr.responseText);
       xhr.send();
     },
   },
