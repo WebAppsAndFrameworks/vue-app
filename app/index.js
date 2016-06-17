532bd43f63a0ba3f882440284cf2b76c26d92b71
Zack Hall <zackhall@outlook.com>
2016-06-17 14:04:40 -0700

    Hello World with Vue JS

--- app/index.js
+++ app/index.js
@@ -4,3 +4,12 @@ require.context('./', true, /\.(png|jpe?g|gif|svg|ico)$/);
 
 // Require styles
 require('./css/styles.scss');
+
+const Vue = require('vue');
+
+new Vue({ // eslint-disable-line no-new
+  el: '#app',
+  data: {
+    message: 'Package Tracking App',
+  },
+});
